9a34be3662a884cffb97feef9a1e918a4aad149f
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-21 16:47:55 +0200

    Test author cannot be null, or blank

--- stream/tests/test_models.py
+++ stream/tests/test_models.py
@@ -52,12 +52,20 @@ class ModelTests(TestCase):
                 content=content
             ).full_clean()
 
-        # Still looking for a cleaner, less brittle way to test this
         self.assertEqual(cm.exception.message_dict, {'content': ['Ensure this value has at most 141 characters (it has 142).']})
 
+    def test_author_cannot_be_null(self):
+        with self.assertRaises(ValidationError) as cm:
+            Purr(content='Content').full_clean()
+
+        self.assertEqual(cm.exception.message_dict, {'author': ['This field cannot be null.']})
+
+    def test_authro_cannot_be_blank(self):
+        with self.assertRaises(ValidationError) as cm:
+            Purr(author='', content='Content').full_clean()
+
+        self.assertEqual(cm.exception.message_dict, {'author': ['This field cannot be blank.']})
 
     # content is allowed special characters
-    # author cannot be null
-    # author cannot be blank
     # author may only contain alphanumeric characters
     # author may not contain non alphanumeric characters
