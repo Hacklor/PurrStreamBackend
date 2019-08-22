ac0c7b5267c7aede113969d850d621d50c27583b
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-22 16:34:28 +0200

    Create instance of Purr model
    Let's try to do this as TDD as possible just to see what happens

--- stream/models.py
+++ stream/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Purr(models.Model):
+
+    def something(self):
+        print('something')
--- stream/tests.py
+++ stream/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from stream.models import Purr
+
+class ModelTests(TestCase):
+
+    def create_instance(self):
+        Purr()
