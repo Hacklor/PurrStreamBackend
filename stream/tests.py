3f25d68896013a4e3bb9278d4a50c719d8ad44aa
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-23 13:55:07 +0200

    Add Content field

--- stream/tests.py
+++ stream/tests.py
@@ -6,15 +6,17 @@ from stream.models import Purr
 
 class ModelTests(TestCase):
 
-    def test_contains_author(self):
+    def test_purr_contains_author_and_content(self):
         purr = Purr(
             author='Author',
+            content='Content',
         )
         purr.clean()
         purr.save()
 
         actual = Purr.objects.first()
         self.assertEquals(actual.author, 'Author')
+        self.assertEquals(actual.content, 'Content')
 
     def test_author_cannot_save_null(self):
         with self.assertRaisesMessage(IntegrityError, 'NOT NULL constraint failed: stream_purr.author'):
@@ -22,32 +24,37 @@ class ModelTests(TestCase):
 
     def test_author_null_is_invalid(self):
         with self.assertRaises(ValidationError) as cm:
-            Purr().full_clean()
-
-        self.assertEqual(cm.exception.message_dict, {'author': ['This field cannot be null.']})
-
-    def test_author_null_is_invalid(self):
-        with self.assertRaises(ValidationError) as cm:
-            Purr().full_clean()
+            Purr(
+                content='Content',
+            ).full_clean()
 
         self.assertEqual(cm.exception.message_dict, {'author': ['This field cannot be null.']})
 
     def test_author_blank_is_invalid(self):
         with self.assertRaises(ValidationError) as cm:
-            Purr(author='').full_clean()
+            Purr(
+                author='',
+                content='Content',
+            ).full_clean()
 
         self.assertEqual(cm.exception.message_dict, {'author': ['This field cannot be blank.']})
 
     def test_author_cannot_be_longer_than_32_chars(self):
         invalid_author = 33 * 'a'
         with self.assertRaises(ValidationError) as cm:
-            Purr(author=invalid_author).full_clean()
+            Purr(
+                author=invalid_author,
+                content='Content',
+            ).full_clean()
 
         self.assertEqual(cm.exception.message_dict, {'author': ['Ensure this value has at most 32 characters (it has 33).']})
 
     def test_author_is_allowed_to_be_32_chars(self):
         valid_author = 32 * 'a'
-        purr = Purr(author=valid_author)
+        purr = Purr(
+            author=valid_author,
+            content='Content',
+        )
         purr.full_clean()
         purr.save()
 
