8a3f039a4c0c648460c950f659fc00a8c0f5cef6
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-24 11:26:45 +0200

    Test retrieving an instance of purr

--- stream/tests/test_purr_serializer.py
+++ stream/tests/test_purr_serializer.py
@@ -19,3 +19,18 @@ class PurrSerializerTests(TestCase):
         self.assertEquals(actual.id, 1)
         self.assertEquals(actual.author, 'Author')
         self.assertEquals(actual.content, 'Content of a purr')
+
+    def test_retrieves_valid_purr_instance(self):
+        purr = Purr(
+            author='Author',
+            content='Content',
+        )
+        purr.save()
+
+        purr_attributes = {
+            'id': 1,
+            'author': 'Author',
+            'content': 'Content',
+        }
+        serializer = PurrSerializer(instance=purr)
+        self.assertEquals(serializer.data, purr_attributes)
