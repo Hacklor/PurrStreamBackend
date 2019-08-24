3de4f2b4f15922abd2e79ca181e9610129e89d43
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-24 11:14:50 +0200

    Serializer creates valid purr instance

--- stream/tests/test_purr_serializer.py
+++ stream/tests/test_purr_serializer.py
@@ -2,8 +2,20 @@ from django.test import TestCase
 from rest_framework import serializers
 
 from stream.serializers import PurrSerializer
+from stream.models import Purr
 
 class PurrSerializerTests(TestCase):
 
-    def test_initialize(self):
-        self.assertIsInstance(PurrSerializer(), serializers.ModelSerializer)
+    def test_creates_valid_purr_instance(self):
+        purr_attributes = {
+            'author': 'Author',
+            'content': 'Content of a purr',
+        }
+        serializer = PurrSerializer(data=purr_attributes)
+        self.assertTrue(serializer.is_valid())
+        actual = serializer.save()
+
+        self.assertIsInstance(actual, Purr)
+        self.assertEquals(actual.id, 1)
+        self.assertEquals(actual.author, 'Author')
+        self.assertEquals(actual.content, 'Content of a purr')
