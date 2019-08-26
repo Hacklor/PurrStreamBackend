0a5540c75fc2b4a523f594822f6a1ed57698a5b3
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-26 11:22:42 +0200

    Returns a list with single purr

--- stream/api.py
+++ stream/api.py
@@ -1,6 +1,14 @@
-from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
+from stream.models import Purr
+from stream.serializers import PurrSerializer
+
 @api_view(['GET'])
+@parser_classes([JSONParser])
 def purr_list(request):
-    return(Response([]))
+    purrs = Purr.objects.all()
+    serializer = PurrSerializer(purrs, many=True)
+    return Response(serializer.data)
--- stream/tests/test_purr_api.py
+++ stream/tests/test_purr_api.py
@@ -1,15 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-from rest_framework.test import APIRequestFactory
+from stream.models import Purr
 
-class PurrApiTests(TestCase):
+class PurrApiTests(APITestCase):
 
     def test_returns_success(self):
-        response = self.client.get('/purrs/', format='json')
+        response = self.client.get('/purrs/')
 
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+        self.assertEquals('application/json', response['Content-Type'])
 
     def test_returns_empty_list_of_purrs(self):
-        response = self.client.get('/purrs/', format='json')
+        response = self.client.get('/purrs/')
 
-        self.assertEquals(response.data, [])
+        self.assertEquals([], response.data)
+
+    def test_returns_purr_when_present(self):
+        Purr.objects.create(author='Author', content='Content')
+
+        response = self.client.get('/purrs/')
+
+        expected = [ {'id': 1, 'author': 'Author', 'content': 'Content'} ]
+        self.assertEquals(expected, response.data)
